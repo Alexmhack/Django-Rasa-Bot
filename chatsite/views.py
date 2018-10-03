d7e2b5a717cdbfd1fc9699804d764091bf9e9e43
alexHooper <alexmohack@gmail.com>
2018-10-03 21:16:09 +0530

    add csrf exempt to view

--- chatsite/views.py
+++ chatsite/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .bot import agent
 
 def home_view(request):
 	return render(request, 'home_view.html')
+
+
+@csrf_exempt
+def handle_response(request, *args, **kwargs):
+	if request.method == "POST":
+		print(request)
+		print(kwargs)
+		responses = agent.handle_message(message)
+		print(responses)
+		return JsonResponse(responses)
