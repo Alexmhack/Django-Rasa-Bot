813658611fcb15bec86a76177257dd8f42fb40e7
alexHooper <alexmohack@gmail.com>
2018-10-03 17:44:27 +0530

    add corsheaders in settings

--- chatsite/settings.py
+++ chatsite/settings.py
@@ -39,11 +39,19 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # django-cors-headers
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    # middleware for corsheaders
+    'corsheaders.middleware.CorsMiddleware',
+
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
