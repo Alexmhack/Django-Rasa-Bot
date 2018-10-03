8faf33c012b98404152396108281d2ef92c982be
alexHooper <alexmohack@gmail.com>
2018-10-03 16:33:35 +0530

    create env file

--- chatsite/settings.py
+++ chatsite/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+from decouple import config
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5(06l%yqgyr@^$!y3o%d1%7gd_2!y=!zjubypfk_3o)h)^wvkx'
+SECRET_KEY = config("PROJECT_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
