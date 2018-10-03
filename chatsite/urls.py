8098ae60b9a59495fa70b9aa16fa533a06b3edef
alexHooper <alexmohack@gmail.com>
2018-10-03 16:40:11 +0530

    add url for home view

--- chatsite/urls.py
+++ chatsite/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import home_view
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('', home_view, name='home'),
 ]
