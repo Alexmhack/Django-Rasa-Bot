0015a21f7d66011f4e4dad1892c0e556be1397cf
alexHooper <alexmohack@gmail.com>
2018-10-03 17:16:13 +0530

    send message from bot

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,16 @@
 var botui = new BotUI('my-botui-app');
+
+botui.message.add({ // show a message
+  human: true,
+  content: 'Whats your name?'
+}).then(function () { // wait till its shown
+  return botui.action.text({ // show 'text' action
+    action: {
+      placeholder: 'Your name'
+    }
+  });
+}).then(function (res) { // get the result
+  botui.message.add({
+    content: 'Your name is ' + res.value
+  });
+});
