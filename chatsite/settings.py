29758b4c279199e480404decafe1433a6b147f12
alexHooper <alexmohack@gmail.com>
2018-10-03 16:34:50 +0530

    change local time

--- chatsite/settings.py
+++ chatsite/settings.py
@@ -107,13 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Kolkata'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
