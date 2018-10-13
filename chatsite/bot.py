f5ac012514fd8fa68ae413050ed46ee18b414bcb
alexHooper <alexmohack@gmail.com>
2018-10-13 20:27:25 +0530

    add rasa socket settings

--- chatsite/bot.py
+++ chatsite/bot.py
@@ -1,17 +1,26 @@
 from django.conf import settings
 
 from rasa_core.agent import Agent
-from rasa_addons.webchat import WebChatInput, SocketInputChannel
-
-messages = dict()
+from rasa_core.channels.socketio import SocketIOInput
+from rasa_core.agent import Agent
 
 MODELS = settings.RASA_CORE_MODELS
 NLU = settings.RASA_CORE_NLU
 
+# load your trained agent
 agent = Agent.load(MODELS, interpreter=NLU)
 
-input_channel = WebChatInput(static_assets_path=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates'))
-agent.handle_channel(SocketInputChannel(5500, "/bot", input_channel))
+input_channel = SocketIOInput(
+	# event name for messages sent from the user
+	user_message_evt="user_uttered",
+	# event name for messages sent from the bot
+	bot_message_evt="bot_uttered",
+	# socket.io namespace to use for the messages
+	namespace=None
+)
+
+# set serve_forever=False if you want to keep the server running
+s = agent.handle_channels([input_channel], 5500, serve_forever=True)
 
 # while True:
 # 	print([m for m in messages])
