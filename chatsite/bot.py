426fd85aac78280331134c79ff23414830cd23fb
alexHooper <alexmohack@gmail.com>
2018-10-13 19:57:53 +0530

    add input layer with rasa addons

--- chatsite/bot.py
+++ chatsite/bot.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 
 from rasa_core.agent import Agent
+from rasa_addons.webchat import WebChatInput, SocketInputChannel
 
 messages = dict()
 
@@ -8,6 +9,9 @@ MODELS = settings.RASA_CORE_MODELS
 NLU = settings.RASA_CORE_NLU
 
 agent = Agent.load(MODELS, interpreter=NLU)
+
+input_channel = WebChatInput(static_assets_path=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates'))
+agent.handle_channel(SocketInputChannel(5500, "/bot", input_channel))
 
 # while True:
 # 	print([m for m in messages])
