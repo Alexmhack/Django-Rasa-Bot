5d557f7c474d6928ac0164e757bb29f0f30994f2
alexHooper <alexmohack@gmail.com>
2018-10-13 19:58:39 +0530

    comment out the botui script

--- static/js/script.js
+++ static/js/script.js
@@ -1,40 +1,45 @@
 var botui = new BotUI('my-botui-app');
 
+$("#chatForm").submit((e) => {
+	// e.preventDefault();
+	console.log('script is running...')
+
+	var formData = $("#chatForm").serialize();
+	var userInput = $("#userInput").val();
+	
+	sendMessage(formData);
+})
+
 function sendMessage(message) {
-	console.log('sendMessage is working')
-	var data = {
-		'q': message
-	};
+	console.log('sendMessage is working with message: ' + message)
 
 	$.ajax({
-		type: "POST",
-		url: "http://localhost:8000/chat/",
-		dataType: 'json',
-		data: {
-			'msg': message
-		},
-		success: (res) => {
-			console.log(res.get('text'))
+		url: "/chat/",
+		data: message,
+		method: "POST",
+		success: (data) => {
+			console.log(data);
+			$(this).find("input[type=text], textarea").val("")
 		},
 		error: (err) => {
 			console.error(err);
 			console.error(err.status);
 			console.error(err.statusText);
 		}
-	});
+	})
 }
 
-botui.message.add({ // show a message
-  human: true,
-  content: 'Hey there! I am Django Bot'
-}).then(function () { // wait till its shown
-  return botui.action.text({ // show 'text' action
-    action: {
-      placeholder: 'Enter your message here...'
-    }
-  });
-}).then(function (res) { // get the result
-  botui.message.add({
-    content: sendMessage(res)// sendMessage(message);
-  });
-});
+// botui.message.add({ // show a message
+//   human: true,
+//   content: 'Hey there! I am Django Bot'
+// }).then(function () { // wait till its shown
+//   return botui.action.text({ // show 'text' action
+//     action: {
+//       placeholder: 'Enter your message here...'
+//     }
+//   });
+// }).then(function (res) { // get the result
+//   botui.message.add({
+//     content: sendMessage(res.value)// sendMessage(message);
+//   });
+// });
